dd34e0dfe1594f76bf33f6ab5bcbc4c3c9eeaf87
shayaansiddiqui <shayaan.siddiqui@gmail.com>
2023-12-01 13:45:16 -0500

    final migration to Tailwind

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import {styled} from 'styled-components';
-import ControlButton from "./Button";
+import Button from "./Button";
 import CustomInput from "./Input";
 
 const ControlDiv = styled.div`
@@ -31,8 +31,8 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <ControlDiv>
+    <div id="auth-inputs" className={'w-full max-w-sm p-8 mx-auto rounded shadow-md gb-gradient-to-b from-stone-700 from-stone-800'}>
+      <div className={'flex flex-col gap-2 mb-6'}>
           <CustomInput
                label={'Email'}
                invalid={emailNotValid}
@@ -46,12 +46,12 @@ export default function AuthInputs() {
                             handleInputChange('password', event.target.value)
                        }
           />
-      </ControlDiv>
-      <div className="actions">
-        <button type="button" className="text-button">
+      </div>
+      <div className="flex justify-end gap-4">
+        <button type="button" className="text-amber-400 hover:text-amber-500">
           Create a new account
         </button>
-        <ControlButton onClick={handleLogin}>Sign In</ControlButton>
+        <Button onClick={handleLogin}>Sign In</Button>
       </div>
     </div>
   );
